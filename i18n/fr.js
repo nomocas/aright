5a2e58a7d35695209abdebb3d242a87076723a43
Gilles Coomans <gilles.coomans@gmail.com>
2015-10-07 22:32:05 +0200

    add dist and i18n folder

---
+++ i18n/fr.js
@@ -0,0 +1,17 @@
+module.exports = {
+	"string": "n'est pas une string",
+	"object": "n'est pas un objet",
+	"array": "n'est pas une array",
+	"boolean": "n'est pas un boolean",
+	"number": "n'est pas un nombre",
+	"null": "devrait être null",
+	"enum": "enumerable non respecté (devrait être un de : %s)",
+	"equal": "égalité non vérifié (devrait être égal à : %s)",
+	"format": "format non respecté",
+	"unmanaged": "propriété non prévue",
+	"missing": "propriété manquante",
+	"minLength": "trop court (la longueur devrait être au minimum : %s)",
+	"maxLength": "trop long (la longueur devrait être au maximum : %s)",
+	"minimum": "trop petit (devrait être au minimum : %s)",
+	"maximum": "trop grand (devrait être au maximum : %s)"
+};
