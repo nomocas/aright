f68e67fb30a00b8dfc230abdc1a9669e9747a79f
Gilles Coomans <gilles.coomans@gmail.com>
2015-10-08 15:21:28 +0200

    correct email format pattern

--- index.js
+++ index.js
@@ -56,8 +56,7 @@
 	};
 
 	var formats = {
-		email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/gi,
-		phone: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/gi
+		email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 	};
 
 	function is(type) {
